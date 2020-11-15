55a2e8587e2bd0ca4af7d35317ba13a125c50480
Benjamin Inemugha <binemugha@gmail.com>
2020-11-15 21:27:01 +0100

    Initial Commit

---
+++ Stratt Splash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stratt Splash
+//
+//  Created by Benjamin Inemugha on 15/11/2020.
+//  Copyright © 2020 Techelope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
